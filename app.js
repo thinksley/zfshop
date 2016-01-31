5b61ad1484a0be7d1e0e506271c791b031ac03b8
thinksLey <939480781@qq.com>
2016-01-31 21:54:42 +0800

    用户登录注册

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 var express =require('express');
 var path=require('path');
 var app=express();
-var bodyParser=require('body-parser');
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/zhufengshop');
+
+var bodyParser = require('body-parser');
+var users = require('./routes/users');
+
 // 静态服务器中间件
 app.use(express.static(path.join(__dirname,'app','public')));
 app.use(bodyParser.json()); //解析json请求
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(session({
+    secret:'thinksley',
+    resave:true,
+    saveUninitialized:true,
+    cookie:{
+        maxAge:60*60*100
+    },
+    store:new MongoStore({
+        url:'mongodb://127.0.0.1/zhufengshop'
+    })
+}));
 
 app.use('/users',users);
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,54 @@
 var express =require('express');
 var router=express.Router();
+//var crypto = require('crypto');
+var User = require('../models').User;
 
+function encrypt(str){
+	return crypto.createHash('md5').update(str).digest('hex');
+}
 router.post('/reg',function(req,res){
 	var user=req.body;
+	//user.password = encrypt(user.password);
+	//var md5Email = encrypt(user.email);
+	//user.avatar = "https://secure.gravatar.com/avatar/"+md5Email+"?s=48";
+	user.avatar = "https://secure.gravatar.com/avatar/?s=48";
+
+	new User(user).save(function(err,user){
+		console.error(user);
+		if(err){
+			res.status(500).json({msg:err});
+		}else{
+			res.json(user);
+		}
+	});
 })
+
+router.post('/login',function(req,res){
+	var user=req.body;
+	User.findOne({username:user.username,password:user.password},function(err,user){
+		if(err){
+			res.status(500).json({msg:err});
+		}else{
+			req.session.user = user;
+			res.json(user);
+		}
+	})
+
+})
+
+router.post('/validate',function(req,res){
+	var user = req.session.user;
+	if(user && user._id){
+		res.status(200).json(user)
+	}else{
+		res.status(401).json({msg:'用户未登陆'});
+	}
+});
+
+router.post('/logout',function(req,res){
+	req.session.user = null;
+	res.status(200).json({msg:'success'})
+});
+
+
+module.exports = router;
