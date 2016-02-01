518d78d731ae8c083b8ce77369e0e8821db18d9f
thinksLey <939480781@qq.com>
2016-02-01 23:44:48 +0800

    图片添加

--- routes/wares.js
+++ routes/wares.js
@@ -1,24 +1,44 @@
 var express =require('express');
 var router=express.Router();
-//var crypto = require('crypto');
+//var uuid = require('uuid');
+var parser = require('multer')().single('imgSrc');
+
 var Ware = require('../models').Ware;
-
-
+var mime = require('mime');
+var uuid=require('uuid');
+var fs = require('fs');
 //添加商品
-router.post('/add',function(req,res){
+router.post('/add',parser,function(req,res){
     var ware=req.body;
-    console.log(ware)
-    new Ware({
-        name:ware.name,
-        price:ware.price
-    }).save(function(err,ware){
+    var imgInfos=ware.imgSrc.split(',')
+    var ext = mime.extension(imgInfos[0].slice(imgInfos[0].indexOf(":") + 1, imgInfos[0].indexOf(";")));
+    var imgSrc = uuid.v4() + '.' + ext;
+    fs.writeFile('./app/public/upload/' + imgSrc, imgInfos[1], 'base64', function (err, result) {
+        new Ware({
+            name:ware.name,
+            price:ware.price,
+            imgSrc:ware.imgSrc
+        }).save(function(err,ware){
 
-        if(err){
-            res.status(500).json({msg:err});
-        }else{
-            res.json(ware);
+                if(err){
+                    res.status(500).json({msg:err});
+                }else{
+                    res.json(ware);
+                }
+            });
+    })
+
+})
+
+
+router.get('/list',function(req,res){
+    Ware.find({},function(err,wares){
+        if (err) {
+            res.status(500).json({msg: err});
+        } else {
+            res.json(wares);
         }
     });
-})
+});
 
 module.exports = router;
