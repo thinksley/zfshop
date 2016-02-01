d5ccb134812c5a61f7d922ed64feef983dbf679b
thinksLey <939480781@qq.com>
2016-02-01 22:09:05 +0800

    可以提交商品名称和价格

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/zhufengshop');
 
 var bodyParser = require('body-parser');
-var users = require('./routes/users');
-var wares = require('./routes/wares');
+
+
 // 静态服务器中间件
 app.use(express.static(path.join(__dirname,'app','public')));
 app.use(bodyParser.json()); //解析json请求
 app.use(bodyParser.urlencoded({extended:false}));
+
+var wares = require('./routes/wares');
+var users = require('./routes/users');
+
 
 app.use(session({
     secret:'thinksley',
--- routes/wares.js
+++ routes/wares.js
@@ -3,22 +3,22 @@ var router=express.Router();
 //var crypto = require('crypto');
 var Ware = require('../models').Ware;
 
-//添加商品
-router.post('/add', function (req, res) {
-    var ware = req.body;
-    console.log(ware);
-        new Ware({
-            name: ware.name,
-            price: ware.price,
-            imgSrc: 'http://www.baidu.com'
-        }).save(function (err, ware) {
-                if (err) {
-                    res.status(500).json({msg: err});
-                } else {
-                    res.json(ware);
-                }
-            });
 
-});
+//添加商品
+router.post('/add',function(req,res){
+    var ware=req.body;
+    console.log(ware)
+    new Ware({
+        name:ware.name,
+        price:ware.price
+    }).save(function(err,ware){
+
+        if(err){
+            res.status(500).json({msg:err});
+        }else{
+            res.json(ware);
+        }
+    });
+})
 
 module.exports = router;
